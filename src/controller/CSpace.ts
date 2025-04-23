353efec1e8637369086b7467dfce7868103b12f9
surfish <shrshr2@163.com>
2025-04-23 22:24:19 +0800

    clear

--- src/controller/CSpace.ts
+++
@@ -1,3 +0,0 @@
-import { MSpace } from "../model/MSpace";
-
-export const WorkSpace = new MSpace();
--- src/model/MSpace.ts
+++
@@ -1,9 +0,0 @@
-import { MPower } from "./MPower";
-import { NodeID } from "./MSchema";
-
-export class MSpace{
-  powers: Map<NodeID, MPower>;
-  constructor(){
-    this.powers = new Map();
-  }
-}
